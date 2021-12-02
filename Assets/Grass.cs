1ae52af3862ffba1bc19f3c732e3f22f92c3d0ac
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-12-02 04:39:23 -0800

    Add empty script for dispatching grass compute shader

---
+++ Assets/Grass.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour {
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
