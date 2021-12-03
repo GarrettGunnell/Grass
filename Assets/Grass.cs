286100532f4f3358eef4b88dcc82b9b763fa170c
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-12-02 23:34:37 -0800

    Initialize grass positions

--- Assets/Grass.cs
+++ Assets/Grass.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Grass : MonoBehaviour {
-    
+    public int resolution = 100;
+
+    private ComputeShader initializeGrassShader;
+    private ComputeBuffer grassPositionsBuffer;
+
     void Start() {
-        
+        initializeGrassShader = Resources.Load<ComputeShader>("GrassPoint");
+        grassPositionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+
+        initializeGrassShader.SetFloat("_Dimension", resolution);
+        initializeGrassShader.SetBuffer(0, "_GrassPositionsBuffer", grassPositionsBuffer);
+        initializeGrassShader.Dispatch(0, Mathf.CeilToInt(resolution / 8.0f), Mathf.CeilToInt(resolution / 8.0f), 1);
     }
 
     void Update() {
         
+    }
+    
+    void OnDisable() {
+        grassPositionsBuffer.Release();
+        grassPositionsBuffer = null;
     }
 }
