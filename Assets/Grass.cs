7282385ad4a411e28bf8063f1c7022d913e7a3b8
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-12-06 23:59:01 -0800

    Stop repeat code

--- Assets/Grass.cs
+++ Assets/Grass.cs
@@ -9,12 +9,12 @@ public class Grass : MonoBehaviour {
     public Material grassMaterial;
     public Mesh grassMesh;
     public Texture heightMap;
-    public Texture saturationMap;
-
+    
     public bool updateGrass;
 
     private ComputeShader initializeGrassShader;
     private ComputeBuffer grassDataBuffer, argsBuffer;
+    private Material grassMaterial2, grassMaterial3;
 
     private struct GrassData {
         public Vector4 position;
@@ -23,24 +23,18 @@ public class Grass : MonoBehaviour {
     void Start() {
         resolution *= scale;
         initializeGrassShader = Resources.Load<ComputeShader>("GrassPoint");
-        grassDataBuffer = new ComputeBuffer(resolution * resolution, 4 * 5);
+        grassDataBuffer = new ComputeBuffer(resolution * resolution, 4 * 4);
 
+        updateGrassBuffer();
+    }
+
+    void updateGrassBuffer() {
         initializeGrassShader.SetInt("_Dimension", resolution);
         initializeGrassShader.SetInt("_Scale", scale);
         initializeGrassShader.SetBuffer(0, "_GrassDataBuffer", grassDataBuffer);
         initializeGrassShader.SetTexture(0, "_HeightMap", heightMap);
         initializeGrassShader.SetFloat("_DisplacementStrength", displacementStrength);
         initializeGrassShader.Dispatch(0, Mathf.CeilToInt(resolution / 8.0f), Mathf.CeilToInt(resolution / 8.0f), 1);
-        grassMaterial.SetBuffer("positionBuffer", grassDataBuffer);
-        /*
-        Vector3[] positions = new Vector3[resolution * resolution];
-
-        grassDataBuffer.GetData(positions);
-
-        foreach (Vector3 pos in positions) {
-            Debug.Log($"{pos.x}, {pos.y}, {pos.z}");
-        }
-        */
 
         uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
         // Arguments for drawing mesh.
@@ -51,30 +45,20 @@ public class Grass : MonoBehaviour {
         args[3] = (uint)grassMesh.GetBaseVertex(0);
         argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
         argsBuffer.SetData(args);
-    }
 
-    void updateGrassBuffer() {
-        initializeGrassShader.SetInt("_Dimension", resolution);
-        initializeGrassShader.SetInt("_Scale", scale);
-        initializeGrassShader.SetBuffer(0, "_GrassDataBuffer", grassDataBuffer);
-        initializeGrassShader.SetTexture(0, "_HeightMap", heightMap);
-        initializeGrassShader.SetTexture(0, "_SaturationMap", saturationMap);
-        initializeGrassShader.SetFloat("_DisplacementStrength", displacementStrength);
-        initializeGrassShader.Dispatch(0, Mathf.CeilToInt(resolution / 8.0f), Mathf.CeilToInt(resolution / 8.0f), 1);
         grassMaterial.SetBuffer("positionBuffer", grassDataBuffer);
+        grassMaterial.SetFloat("_Rotation", 0.0f);
+        grassMaterial2 = new Material(grassMaterial);
+        grassMaterial2.SetBuffer("positionBuffer", grassDataBuffer);
+        grassMaterial2.SetFloat("_Rotation", 50.0f);
+        grassMaterial3 = new Material(grassMaterial);
+        grassMaterial3.SetBuffer("positionBuffer", grassDataBuffer);
+        grassMaterial3.SetFloat("_Rotation", -50.0f);
     }
 
     void Update() {
-        grassMaterial.SetBuffer("positionBuffer", grassDataBuffer);
-        grassMaterial.SetFloat("_Rotation", 0.0f);
         Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial, new Bounds(Vector3.zero, new Vector3(-500.0f, 200.0f, 500.0f)), argsBuffer);
-        Material grassMaterial2 = new Material(grassMaterial);
-        grassMaterial2.SetBuffer("positionBuffer", grassDataBuffer);
-        grassMaterial2.SetFloat("_Rotation", 50.0f);
         Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial2, new Bounds(Vector3.zero, new Vector3(-500.0f, 200.0f, 500.0f)), argsBuffer);
-        Material grassMaterial3 = new Material(grassMaterial);
-        grassMaterial3.SetBuffer("positionBuffer", grassDataBuffer);
-        grassMaterial3.SetFloat("_Rotation", -50.0f);
         Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial3, new Bounds(Vector3.zero, new Vector3(-500.0f, 200.0f, 500.0f)), argsBuffer);
 
         if (updateGrass) {
